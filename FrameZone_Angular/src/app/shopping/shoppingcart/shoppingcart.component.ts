6ffe60a71629de6f4bde215e8ba80f40dd24bc32
sen71174123 <cc92furau@gmail.com>
2025-12-11 14:42:54 +0800

    修車2

--- FrameZone_Angular/src/app/shopping/shoppingcart/shoppingcart.component.ts
+++ FrameZone_Angular/src/app/shopping/shoppingcart/shoppingcart.component.ts
@@ -3,23 +3,23 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HeaderComponent } from "../../shared/components/header/header.component";
 
-interface CartItem {
-  id: number;
-  name: string;
-  price: number;
-  quantity: number;
-  selected: boolean; // <-- 關鍵: 用來追蹤是否被勾選
-  // 您可以加入其他屬性，例如: quantity: number;
-}
+// interface CartItem {
+//   id: number;
+//   name: string;
+//   price: number;
+//   quantity: number;
+//   selected: boolean; // <-- 關鍵: 用來追蹤是否被勾選
+//   // 您可以加入其他屬性，例如: quantity: number;
+// }
 
-interface Coupon {
-  id: number;
-  name: string;
-  discount: number; // 折扣金額
-  code: string;
-  expiryDate: Date;
-  isSelected: boolean; // 是否被使用者選中
-}
+// interface Coupon {
+//   id: number;
+//   name: string;
+//   discount: number; // 折扣金額
+//   code: string;
+//   expiryDate: Date;
+//   isSelected: boolean; // 是否被使用者選中
+// }
 
 @Component({
   selector: 'app-shoppingcart',
