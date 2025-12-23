7c13830e95e88119a9532e4a300c436226d69a34
Maple <maplework49@gmail.com>
2025-12-23 19:12:21 +0800

    fix:修復user-menu & navbar

--- FrameZone_Angular/src/app/shared/components/header/header.component.ts
+++ FrameZone_Angular/src/app/shared/components/header/header.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component, HostListener } from '@angular/core';
+import { Router, RouterModule, NavigationEnd } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { ThemeToggleComponent } from '../theme-toggle/theme-toggle.component';
 import { UserMenuComponent } from '../user-menu/user-menu.component';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-header',
@@ -11,6 +12,41 @@ import { UserMenuComponent } from '../user-menu/user-menu.component';
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  isMenuOpen: boolean = false;
 
+  constructor(private router: Router) {
+    // 監聽路由變化，當路由改變時關閉選單
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe(() => {
+        this.closeMenu();
+      });
+  }
+
+  /**
+   * 切換選單開關狀態
+   */
+  toggleMenu(): void {
+    this.isMenuOpen = !this.isMenuOpen;
+    this.updateBodyScroll();
+  }
+
+  /**
+   * 關閉選單
+   */
+  closeMenu(): void {
+    this.isMenuOpen = false;
+    this.updateBodyScroll();
+  }
+
+  /**
+   * 更新 body 滾動狀態
+   */
+  private updateBodyScroll(): void {
+    if (this.isMenuOpen) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = '';
+    }
+  }
 }
-
