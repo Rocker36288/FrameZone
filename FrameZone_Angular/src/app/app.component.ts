01fda02b72ebfc7cf4bbef52240d623801ba1dd5
blueblue999 <blueblue969696@gmail.com>
2026-01-03 01:04:24 +0800

    一直出現 的小問題

--- FrameZone_Angular/src/app/app.component.ts
+++ FrameZone_Angular/src/app/app.component.ts
@@ -1,16 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ShoppingcartComponent } from "./shopping/shoppingcart/shoppingcart.component";
-import { ShoppingHeaderComponent } from "./shopping/shopping-header/shopping-header.component";
-import { ShoppingCheckoutComponent } from "./shopping/shopping-checkout/shopping-checkout.component";
-import { ShoppinghomeComponent } from "./shopping/shoppinghome/shoppinghome.component";
-import { ShoppingProductDetailComponent } from "./shopping/shopping-product-detail/shopping-product-detail.component";
-import { ShoppingSellershopComponent } from "./shopping/shopping-sellershop/shopping-sellershop.component";
-import { ShoppingBuyerCenterComponent } from "./shopping/shopping-buyer-center/shopping-buyer-center.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ShoppingcartComponent, ShoppingHeaderComponent, ShoppingCheckoutComponent, ShoppinghomeComponent, ShoppingProductDetailComponent, ShoppingSellershopComponent, ShoppingBuyerCenterComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
