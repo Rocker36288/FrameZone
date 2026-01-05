05b8e850e1732518bd8e8b7f8b85e7c29d600562
sen71174123 <cc92furau@gmail.com>
2026-01-05 14:09:55 +0800

    商品圖

---
+++ FrameZone_Angular/src/app/shopping/shared/configuration/url.ts
@@ -0,0 +1,13 @@
+
+// export var frontendPublicUrl: string = 'https://1cd4nrpd-4200.asse.devtunnels.ms';
+
+export var frontendPublicUrl: string = 'https://0k2p097z-4200.asse.devtunnels.ms';
+
+
+// export var backendPublicUrl: string = 'https://xlsf3q5n-7213.asse.devtunnels.ms';
+
+export var backendPublicUrl: string = 'https://74jcr1kn-7213.asse.devtunnels.ms';
+
+//如果要在自己的電腦上執行，要用微軟工具devtunnel取得自己的公開url修改上面兩行參數，前後端都要
+
+//如果用ngrok的Url要另外下載，只要設後端
